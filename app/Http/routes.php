6eed6c360e3e10ab2c437413b688c4a7ae9cbf06
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2016-10-11 23:03:37 +0100

    AUS: live, removed nz routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -97,11 +97,11 @@ Route::group(['domain' => '127.0.0.1'], function()
 
 
 
-*/
+
 
 Route::group(['domain' => 'getaustraliastanding.org'], function()
 {
-    //Route::auth();
+    Route::auth();
     Route::get('/jobs', 'HomeController@jobs');
 
     Route::get('/popup_confirm','GausPopupController@confirm');
@@ -109,7 +109,7 @@ Route::group(['domain' => 'getaustraliastanding.org'], function()
     Route::post('popup_reg', 'GausPopupController@store');
     Route::post('gaus_reg', 'GausController@store');
 });
-
+*/
 
 
 Route::group(['domain' => 'getnzstanding.net'], function()
