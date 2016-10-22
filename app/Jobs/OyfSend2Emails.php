25165863fa7e9071f2fa83b8350c778042eab7a2
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2016-10-22 17:50:19 +0100

    OYF DAY: now ready live

--- app/Jobs/OyfSend2Emails.php
+++ app/Jobs/OyfSend2Emails.php
@@ -45,7 +45,8 @@ class OyfSend2Emails extends Job implements ShouldQueue
 
         Mail::send('emails.oyf.oyf_reg_notify', $array, function ($message) use ($tosend){
             $message->from('registrations@activeworking.com', 'On Your Feet Day');
-            $message->to('web@activeworking.com');
+            $message->to('info@activeworking.com');
+            $message->cc('web@activeworking.com');
             $message->subject('On Your Feet Registration');
         });
     }
