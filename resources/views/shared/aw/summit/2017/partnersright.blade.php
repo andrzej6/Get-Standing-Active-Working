f52d7fe42445d7192692fd4467a0daf0e9d04881
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2016-11-07 14:01:57 +0000

    aw added yo-yo to supporters

--- resources/views/shared/aw/summit/2017/partnersright.blade.php
+++ resources/views/shared/aw/summit/2017/partnersright.blade.php
@@ -30,6 +30,12 @@
             </a>
         </p>
 
+        <p>
+            <a href="https://sit-stand.com/23_yo-yo-desk" target="_blank">
+                <img src="{{ url('/') }}/img/aw/summit/partners/yo-yo.jpg">
+            </a>
+        </p>
+
     </div>
 
 
