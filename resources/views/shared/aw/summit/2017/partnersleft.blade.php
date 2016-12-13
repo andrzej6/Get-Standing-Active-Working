2159b1d057966e943d3c09ac17a1257b35411278
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2016-12-13 13:31:25 +0000

    aw summit changes12

--- resources/views/shared/aw/summit/2017/partnersleft.blade.php
+++ resources/views/shared/aw/summit/2017/partnersleft.blade.php
@@ -3,7 +3,7 @@
     <div class="aw-partners-title">HOST</div>
 
     <div class="aw-partners-platinum">
-
+   <br/>
         <p>
             <a href="http://www.klgates.com/" target="_blank">
                 <img src="{{ url('/') }}/img/aw/summit/partners/klgates.jpg" width="150px" height="100px">
