b9cdf573231973700864784c79824ec6ac07892e
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2016-12-14 17:07:12 +0000

    aw summit changes38

---
+++ resources/views/aw/summit/2017/brochure.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.aw.summit-2017notop')
+
+@section('header-styles')
+    @parent
+@endsection
+
+
+@section('description')
+    Active Working provide thought leadership to reduce prolonged sitting in the workplace. Working with government,
+    medical science, health charities and employers.
+@endsection
+
+@section('keywords')
+    active-working, productive-working, office-productivity, sit-stand, standing-desk, stand-up desk, sit-n-stand,
+    wellness, office-wellness, employee-wellness, sedentary-office, wellbeing, physical-activity, employee-engagement,
+    staff-resilience, stress-management, productivity, occupational-health, height-adjustable-desk, healthy-office, health-and-safety,
+    office-wellbeing, well-being
+@endsection
+
+@section('title')
+    Active Working Summit 2017. Brochure
+@endsection
---
+++ resources/views/layouts/aw/summit-2017notop.blade.php
@@ -0,0 +1,31 @@
+{{-- */
+$sidemenu=array('contact'=>'Contact','pricing'=>'Pricing','book'=>'Book Now','venue'=>'Venue');
+/* --}}
+
+@extends('layouts.aw.summit')
+
+@section('header-styles')
+    @parent
+    <link rel="stylesheet" type="text/css" href="{{ url('/') }}/css/aw/summit17.css">
+@endsection
+
+@section('left-sidemenu')
+    @foreach ($sidemenu as $key=>$value)
+        <a href="{{ url('/') }}/summit/2017/{{$key}}"><li class="@if($key==$page) active @endif">{{$value}}</li></a>
+    @endforeach
+@endsection
+
+@section('partnersleft')
+    @include('shared.aw.summit.2017.partnersleft')
+@endsection
+
+@section('top-content')
+    <div class="center-sidemenu">
+         <img src="{{ url('/') }}/img/aw/summit/gb/2017/brochurepic.jpg" >
+    </div>
+@endsection
+
+
+@section('partnersright')
+    @include('shared.aw.summit.2017.partnersright')
+@endsection
