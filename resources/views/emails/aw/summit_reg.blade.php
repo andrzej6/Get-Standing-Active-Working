0f9d0a963815a3da33b21f8f0feb774c80a6ddf7
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2016-12-15 16:10:04 +0000

    aw summit changes43

--- resources/views/emails/aw/summit_reg.blade.php
+++ resources/views/emails/aw/summit_reg.blade.php
@@ -49,7 +49,7 @@
 
     </div>
 
-    @include('shared.aw.email_supporters')
+    @include('shared.aw.summit_supporters')
 
 </div>
 </body>
---
+++ resources/views/shared/aw/summit/summit_supporters.blade.php
@@ -0,0 +1,56 @@
+<div>
+    Active Working Summit is proud to be associated with the following
+    <strong>"Official Partners"</strong>: <br/>
+
+    <a href="http://www.klgates.com/" style="color:#009999;">
+        <img src="{{ url('/') }}/img/aw/summit/partners/klgates.jpg" width="150"/></a>&nbsp;&nbsp;&nbsp;
+
+    <a href="http://sit-stand.com/" style="color:#009999;">
+        <img src="{{ url('/') }}/img/general/emails/supporters/sit-stand.jpg" width="150"/></a>&nbsp;&nbsp;&nbsp;
+
+    <a href="http://www.aeris.de/en/" style="color:#009999;">
+        <img src="{{ url('/') }}/img/aw/summit/partners/aeris.png" width="150"/></a>&nbsp;&nbsp;&nbsp;
+
+    <a href="http://imprintmats.com/" style="color:#009999;">
+        <img src="{{ url('/') }}/img/general/emails/supporters/imprint.jpg" width="150"/></a>&nbsp;&nbsp;&nbsp;
+
+
+
+    <div style="clear:both"></div>
+
+    <a href="https://sit-stand.com/23_yo-yo-desk" style="color:#009999;">
+        <img src="{{ url('/') }}/img/aw/summit/partners/yo-yo1.jpg" width="150"/></a>&nbsp;&nbsp;&nbsp;
+
+
+    <a href="http://www.fellowes.com/gb/en/Pages/default.aspx" style="color:#009999;">
+        <img src="{{ url('/') }}/img/aw/summit/partners/fellowes.jpg" width="150"/></a>&nbsp;&nbsp;&nbsp;
+
+
+    <a href="http://www.steppie.dk/" style="color:#009999;">
+        <img src="{{ url('/') }}/img/aw/summit/partners/steppie.jpg" width="150"/></a>&nbsp;&nbsp;&nbsp;
+
+
+    
+
+
+
+    <br/>
+
+    Thank you from all the team <br/><br/>
+
+    <strong> Active Working C.I.C  <br/>
+
+        <br/><br/>
+
+        <div style="font-size:0.8em;text-align:justify">
+
+            <strong>Active Working CIC </strong>is leading the global insight and evidence based research on
+            sedentary behaviour. We commissioned the first
+            <a href="http://activeworking.com/expert_statement">global expert recommendations </a>
+            on standing time for office workers (with the support of <strong>Public Health England</strong>),
+            published by the <strong>British Journal for Sports Medicine</strong> in June 2015. We spearhead
+            international  <strong>Get Standing &trade;</strong> campaigns in UK, Australia, USA, Canada, Ireland and Europe.
+        </div>
+
+
+</div>
