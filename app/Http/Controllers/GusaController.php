a2d3517efbce982cce056c8e89b2e658df824978
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-01-10 12:05:48 +0000

    sitemap usa sent and removed

--- app/Http/Controllers/GusaController.php
+++ app/Http/Controllers/GusaController.php
@@ -15,10 +15,7 @@ use App\Jobs\UsaSend2Emails;
 
 class GusaController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('sitemap');
-    }
+
 
     public function index($page='index'){
 
