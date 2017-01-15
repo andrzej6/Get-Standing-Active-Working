c678998f9eddce5f90c24c45d196b154008479a6
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-01-15 16:33:33 +0000

    gaus sitemap final fix2

--- app/Http/Middleware/sitemap.php
+++ app/Http/Middleware/sitemap.php
@@ -31,10 +31,35 @@ class sitemap {
     {
         if ( !$request->is("sitemap") && $request->fullUrl() != '' && $this->auth->guest() )
         {
-            $aSiteMap = \Cache::get('sitemap', []);
+
+            $web_url = $request->url();
+            $parsed = parse_url($web_url);
+
+            switch ($parsed) {
+                case "getaustraliastanding.org":
+                    $aSiteMap = \Cache::get('sitemap_aus', []);
+            break;
+
+            }
+
+
+
             $changefreq = 'always';
             if ( !empty( $aSiteMap[$request->fullUrl()]['added'] ) ) {
-                $changefreq = 'monthly';
+                $aDateDiff = Carbon::createFromTimestamp( $aSiteMap[$request->fullUrl()]['added'] )->diff( Carbon::now() );
+                if ( $aDateDiff->y > 0 ) {
+                    $changefreq = 'yearly';
+                } else if ( $aDateDiff->m > 0) {
+                    $changefreq = 'monthly';
+                } else if ( $aDateDiff->d > 6 ) {
+                    $changefreq = 'weekly';
+                } else if ( $aDateDiff->d > 0 && $aDateDiff->d < 7 ) {
+                    $changefreq = 'daily';
+                } else if ( $aDateDiff->h > 0 ) {
+                    $changefreq = 'hourly';
+                } else {
+                    $changefreq = 'always';
+                }
             }
             $aSiteMap[$request->fullUrl()] = [
                 'added' => time(),
@@ -42,7 +67,16 @@ class sitemap {
                 'priority' => 1 - substr_count($request->getPathInfo(), '/') / 10,
                 'changefreq' => $changefreq
             ];
-            \Cache::put('sitemap', $aSiteMap, 2880);
+
+
+            switch ($parsed) {
+                case "getaustraliastanding.org":
+                    \Cache::put('sitemap_aus', $aSiteMap, 2880);
+                    break;
+
+            }
+
+
         }
         return $next($request);
     }
--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -3,7 +3,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
         xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-    @foreach( Cache::get('sitemap') as $url => $params )
+    @foreach( Cache::get('sitemap_aus') as $url => $params )
         <url>
             <loc>{{$url}}</loc>
             <lastmod>{{$params['lastmod']}}</lastmod>
