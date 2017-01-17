99c52f8f973fe881a7e7679ef126688b357280eb
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-01-17 12:17:14 +0000

    new gif page2

---
+++ resources/views/aw/summit/2017/delegates.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.aw.summit-2017')
+
+@section('header-styles')
+    @parent
+@endsection
+
+
+@section('description')
+    Active Working provide thought leadership to reduce prolonged sitting in the workplace. Working with government,
+    medical science, health charities and employers.
+@endsection
+
+@section('keywords')
+    active-working, productive-working, office-productivity, sit-stand, standing-desk, stand-up desk, sit-n-stand,
+    wellness, office-wellness, employee-wellness, sedentary-office, wellbeing, physical-activity, employee-engagement,
+    staff-resilience, stress-management, productivity, occupational-health, height-adjustable-desk, healthy-office, health-and-safety,
+    office-wellbeing, well-being
+@endsection
+
+@section('title')
+    Active Working Summit 2017. Past Delegates
+@endsection
+
+
+@section('aw-maincontent')
+    <br/>
+    <img src="{{ url('/') }}/img/general/icons/giftest.gif" width="230">&nbsp;
+
+    <img src="{{ url('/') }}/img/general/icons/giftest.gif" width="230">&nbsp;
+
+    <img src="{{ url('/') }}/img/general/icons/giftest.gif" width="230">
+
+
+
+@endsection
+
+
+@section('footer-scripts')
+
+@endsection
