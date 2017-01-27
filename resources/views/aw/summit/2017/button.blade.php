c7f5eb929acfad57fb578ca345797e7d00db86ae
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-01-27 17:01:23 +0000

    summit 20% update hidden

--- resources/views/aw/summit/2017/button.blade.php
+++ resources/views/aw/summit/2017/button.blade.php
@@ -69,7 +69,9 @@ $sandbox=0;
 
                                 @if ($supplier =='NO')
                                    <option value="Standard-15% Discount">Early Bird 15% Discount - FULL DAY - £284.75 GBP</option>
+                                <!--
                                     <option value="Standard-20% Discount">Early Bird 20% Discount - FULL DAY - £268.00 GBP</option>
+                                 -->
 
                                 @elseif ($supplier =='YES')
                                    <option value="Suppliers-15% Discount">Early Bird 15% Discount - FULL DAY - £675.75 GBP</option>
