6ef03ef81a4f579e1dbddb54b63a8b6218e0e264
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-02-13 14:21:23 +0000

    oyf email corrections3

--- resources/views/emails/oyf/oyf_reg.blade.php
+++ resources/views/emails/oyf/oyf_reg.blade.php
@@ -11,10 +11,13 @@
     <a href="http://activeworking.com" style="color:#009999;">
         <img src="{{ url('/') }}/img/general/emails/activeworking.jpg" width="100"/></a> <br/>
 
-    Thank you for registering for <strong>On Your Feet Day</strong> taking place on <u>April 28th 2017</u>.
+    Thank you for registering for <strong>On Your Feet Day</strong> taking place on <u>April 28th 2017</u>,
+    in your office and all around the world.
     <br/><br/>
 
-    We will contact you again in March 2017 when all our posters and communication materials will be available for download.
+
+    All posters and communication materials will be available for download early March, we will send you
+    and email update immediately they are ready.
     <br/><br/>
 
     @include('shared.oyf.email_supporters')
