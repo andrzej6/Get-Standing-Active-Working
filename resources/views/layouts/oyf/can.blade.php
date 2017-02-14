cac097e81b15d6f8fea17c51742ca975c5c3e271
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-02-14 09:18:39 +0000

    oyf corrections6

--- resources/views/layouts/oyf/can.blade.php
+++ resources/views/layouts/oyf/can.blade.php
@@ -23,7 +23,15 @@ $sidemenu=array('index'=>'Event','downloads'=>'Downloads');
             <div id="banner1" class="OneByOne_container">
 
                 <div class="oneByOne_item ls_bg1">
-                    <img src="{{ url('/') }}/img/oyf/can/slider/oyf-canada.jpg" alt="" class="go-to-onyourfeet1">
+                    <img src="{{ url('/') }}/img/oyf/can/slider/oyf-canada1.jpg" alt="" class="go-to-onyourfeet-r">
+                </div>
+
+                <div class="oneByOne_item ls_bg1">
+                    <img src="{{ url('/') }}/img/oyf/can/slider/oyf-canada2.jpg" alt="" class="go-to-onyourfeet-r">
+                </div>
+
+                <div class="oneByOne_item ls_bg1">
+                    <img src="{{ url('/') }}/img/oyf/can/slider/oyf-canada3.jpg" alt="" class="go-to-onyourfeet-r">
                 </div>
 
             </div>
