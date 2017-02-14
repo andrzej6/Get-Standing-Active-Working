988cd8929d28b61813bcbc2b495fcad27c03db27
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-02-14 13:12:47 +0000

    oyf aus

---
+++ resources/views/layouts/oyf/aus.blade.php
@@ -0,0 +1,48 @@
+{{-- */
+$sidemenu=array('index'=>'Event','downloads'=>'Downloads');
+/* --}}
+
+@extends('layouts.oyf')
+
+@section('left-sidemenu')
+    @foreach ($sidemenu as $key=>$value)
+        <a href="{{ url('/') }}/australia/{{$key}}"><li class="@if($key==$page) active @endif">{{$value}}</li></a>
+    @endforeach
+    <a href="{{ url('/') }}/signup"><li>Register</li></a>
+@endsection
+
+
+@section('partnersleft')
+    @include('shared.oyf.aus.partnersleft')
+@endsection
+
+
+@section('oyf-slider')
+    <div class="span12 module_cont module_layer_slider">
+        <div class="module_content slider_type1">
+            <div id="banner1" class="OneByOne_container">
+
+                <div class="oneByOne_item ls_bg1">
+                    <img src="{{ url('/') }}/img/oyf/aus/slider/oyf-aus1.jpg" alt="" class="go-to-onyourfeet-r">
+                </div>
+
+                <div class="oneByOne_item ls_bg1">
+                    <img src="{{ url('/') }}/img/oyf/aus/slider/oyf-aus2.jpg" alt="" class="go-to-onyourfeet-r">
+                </div>
+
+
+                <div class="oneByOne_item ls_bg1">
+                    <img src="{{ url('/') }}/img/oyf/aus/slider/oyf-aus3.jpg" alt="" class="go-to-onyourfeet-r">
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+@endsection
+
+
+@section('aw-restofsmall')
+    @include('shared.oyf.aus.partnersright')
+@endsection
---
+++ resources/views/oyf/australia/downloads.blade.php
@@ -0,0 +1,76 @@
+{{-- */
+$downloads = array(
+'1'=>array('header'=>'Event Preparation', 'elem'=> array(
+        '1'=>array('label'=>'Poster A1','title'=>'Workplace Guide','name'=>'a1'),
+        '2'=>array('label'=>'Flyer A2','title'=>'Download Poters Today','name'=>'a2'),
+        '3'=>array('label'=>'Poster A3','title'=>' Workplace Champion Marco','name'=>'a3-1'),
+        '4'=>array('label'=>'Poster A4','title'=>'Workplace Champion Trudi','name'=>'a3-2'),
+        '5'=>array('label'=>'Poster A5','title'=>'Date for your diary','name'=>'a4')
+        )),
+'2'=>array('header'=>'Event Execution (password required)', 'elem'=> array(
+        '1'=>array('label'=>'Poster B1','title'=>'10 Ways to Sit Less at Work','name'=>'b1'),
+        '2'=>array('label'=>'Poster B2','title'=>'Sitting Increases Health Risks','name'=>'b2'),
+        '3'=>array('label'=>'Poster B3','title'=>'Sitting Calculator','name'=>'b3'),
+        '4'=>array('label'=>'Poster B4','title'=>'Benefits of Sitting Less','name'=>'b4'),
+        '5'=>array('label'=>'Poster B5','title'=>'Stand and Deliver','name'=>'b5'),
+        '6'=>array('label'=>'Poster B6','title'=>'Get up Every 30 Minutes','name'=>'b6'),
+        '7'=>array('label'=>'Poster B7','title'=>'Walk and Talk - Ban Emails Today','name'=>'b7'),
+        '8'=>array('label'=>'Poster B8','title'=>'Try Walking Meeting','name'=>'b8'),
+        '9'=>array('label'=>'Poster B9','title'=>'Take Stairs Rather Than Lift','name'=>'b9'),
+        '10'=>array('label'=>'Poster B10','title'=>'Take Every Opportunity to Get Up and Move','name'=>'b10'),
+        '11'=>array('label'=>'Poster B11','title'=>'Team Stretching','name'=>'b11'),
+        '12'=>array('label'=>'Poster B12','title'=>' What will you do?','name'=>'b12')
+)),
+'3'=>array('header'=>'Desk Display', 'elem'=> array(
+        '1'=>array('label'=>'Display C1','title'=>'OYFB Triangle 1','name'=>'c1'),
+        '2'=>array('label'=>'Display C2','title'=>'OYFB Triangle 2','name'=>'c2'),
+        '3'=>array('label'=>'Display C3','title'=>'OYFB Triangle 3','name'=>'c3'),
+        '4'=>array('label'=>'Display C4','title'=>'OYFB Triangle 4','name'=>'c4'),
+)),
+
+'4'=>array('header'=>'Event Completion', 'elem'=> array(
+        '1'=>array('label'=>'Certificate D1','title'=>' OYFB Certificate','name'=>'d1')
+))
+
+);
+/* --}}
+
+@extends('layouts.oyf.aus')
+
+@section('header-styles')
+    @parent
+    <link rel="stylesheet" type="text/css" href="{{ url('/') }}/css/oyf/download.css">
+@endsection
+
+
+@section('description')
+    On Your Feet. Regular minor movement whilst at work is essential for us to
+    keep our bodies healthy, prevent illness and relieve stress. Sit-Stand workstations
+    enabling us to mix it up between periods of sitting with standing.
+@endsection
+
+@section('keywords')
+    on-your-feet, active-working, productive-working, office-productivity, sit-stand,
+    sit-stand-desk, standing-desk, height-adjustable-desk,
+    variable-height-desk, healthy-office, treadmill-desk, desk-riser, desk-mount, home-office,
+    health-and-safety, wellness
+@endsection
+
+@section('title')
+    On Your Feet Australia
+@endsection
+
+
+
+@section('aw-maincontent')
+
+    <h1 class="oyf-newpink">Downloads </h1>
+
+
+    <div class="oyf-downloads-container">
+            <div class="oyf-newblue fbigger fbold fmargb">Downloads will be available in March 2017.</div>
+    </div><!-- container -->
+
+
+@endsection
+
