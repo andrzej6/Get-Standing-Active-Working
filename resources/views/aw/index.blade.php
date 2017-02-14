ba1d0fdbbc0ea80c21592ed7e50750289b2f683e
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-02-14 08:09:54 +0000

    oyf corrections1

--- resources/views/aw/index.blade.php
+++ resources/views/aw/index.blade.php
@@ -45,11 +45,11 @@
                         <div id="banner1" class="OneByOne_container">
 
                             <div class="oneByOne_item ls_bg1">
-                                <img src="{{ url('/') }}/img/getstanding/gb/slider/big/slider2.jpg" alt="" class="go-to go-to-onyourfeet">
+                                <img src="{{ url('/') }}/img/aw/slider/big/2-awgbs.jpg" alt="" class="go-to go-to-summit">
                             </div>
 
                             <div class="oneByOne_item ls_bg1">
-                                <img src="{{ url('/') }}/img/aw/slider/big/2-awgbs.jpg" alt="" class="go-to go-to-summit">
+                                <img src="{{ url('/') }}/img/getstanding/gb/slider/big/slider2.jpg" alt="" class="go-to go-to-onyourfeet">
                             </div>
 
                             <div class="oneByOne_item ls_bg1">
