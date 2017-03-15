bddb4ed980595b5449a8a4608bc2cba3b06f6a5b
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-03-15 16:32:02 +0000

    oyf downloads gb

--- resources/views/emails/oyf/oyf_reg_new.blade.php
+++ resources/views/emails/oyf/oyf_reg_new.blade.php
@@ -7,14 +7,15 @@
         <img src="{{ url('/') }}/img/general/emails/banners/oyf-gb.jpg" /></a>
     <br/><br/><br/>
 
-    Thank you for registering for <strong>On Your Feet Day</strong> taking place on
+
+    <span style="font-size:14.0pt;">Thank you for registering for <strong>On Your Feet Day</strong> taking place:
     <span  style="color:#57b8d9;font-weight: bold">Friday 28th April 2017</span>,
     in your office and all around the world.
     <br/><br/>
 
 
-    All posters and communication materials will be available for download by March 17th
-    using the password <span  style="color:#57b8d9;font-weight: bold">"OYF17"</span>.
+    All posters and communication materials will be available for download by <strong>March 17th 2017</strong>
+    using the password <span  style="color:#57b8d9;font-weight: bold">"OYF17"</span>.</span>
     <br/><br/>
 
     @include('shared.oyf.email_supporters')
