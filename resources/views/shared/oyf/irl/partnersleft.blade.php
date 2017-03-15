81c1008ab7c457ae398b4041c1323cd6a0992fed
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-03-15 20:13:51 +0000

    oyf irl sponsors

--- resources/views/shared/oyf/irl/partnersleft.blade.php
+++ resources/views/shared/oyf/irl/partnersleft.blade.php
@@ -12,15 +12,44 @@
     </div>
 
 
+    <div class="aw-partners-title">GOLD PARTNERS</div>
+
+
+    <div class="aw-partners-gold">
+
+        <p>
+            <a href="http://activeworking.com/littlenudge.php" target="_blank">
+                <img src="{{ url('/') }}/img/oyf/partners/little_nudge.jpg" width="110px">
+            </a>
+        </p>
+
+
+        <p>
+            <a href="https://sit-stand.com/23_yo-yo-desk" target="_blank">
+                <img src="{{ url('/') }}/img/aw/summit/partners/yo-yo1.jpg">
+            </a>
+        </p>
+
+    </div>
+
+
+
     <div class="aw-partners-title">SILVER PARTNERS</div>
 
     <div class="aw-partners-silver">
+
         <p>
-            <a href="https://sit-stand.com/23_yo-yo-desk" target="_blank">
-                <img src="{{ url('/') }}/img/oyf/partners/yo-yo1.jpg">
+            <a href="http://imprintmats.com/" target="_blank">
+                <img src="{{ url('/') }}/img/oyf/partners/imprint.png" width="110">
             </a>
         </p>
-        <br/>
+
+        <p>
+            <a href="https://steppie.dk/en/" target="_blank">
+                <img src="{{ url('/') }}/img/aw/summit/partners/steppie.jpg" width="110px">
+            </a>
+        </p>
+
 
     </div>
 
