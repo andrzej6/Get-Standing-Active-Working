8074248b573762ff9e425cac907d62db8d672b9d
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-03-15 20:04:46 +0000

    oyf nirl sponsors

--- resources/views/shared/oyf/nirl/partnersleft.blade.php
+++ resources/views/shared/oyf/nirl/partnersleft.blade.php
@@ -34,6 +34,13 @@
             </a>
         </p>
 
+
+        <p>
+            <a href="https://sit-stand.com/23_yo-yo-desk" target="_blank">
+                <img src="{{ url('/') }}/img/oyf/partners/yo-yo1.jpg">
+            </a>
+        </p>
+
     </div>
 
 
@@ -47,17 +54,17 @@
                 <img src="{{ url('/') }}/img/oyf/partners/bhwa1.jpg">
             </a>
         </p>
+        
 
         <p>
-            <a href="https://sit-stand.com/23_yo-yo-desk" target="_blank">
-                <img src="{{ url('/') }}/img/oyf/partners/yo-yo1.jpg">
+            <a href="http://imprintmats.com/" target="_blank">
+                <img src="{{ url('/') }}/img/oyf/partners/imprint.png" width="120">
             </a>
         </p>
 
-
         <p>
-            <a href="http://www.ergotron.com/" target="_blank">
-                <img src="{{ url('/') }}/img/oyf/partners/ergotron.jpg">
+            <a href="https://steppie.dk/en/" target="_blank">
+                <img src="{{ url('/') }}/img/aw/summit/partners/steppie.jpg">
             </a>
         </p>
 
