6993ddc80efde08fe156938caf3535a0a34b41b9
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-03-17 10:12:07 +0000

    oyf email conf update

--- resources/views/emails/oyf/oyf_reg_new.blade.php
+++ resources/views/emails/oyf/oyf_reg_new.blade.php
@@ -14,8 +14,8 @@
     <br/><br/>
 
 
-    All posters and communication materials will be available for download by <strong>March 17th 2017</strong>
-    using the password <span  style="color:#57b8d9;font-weight: bold">"OYF17"</span>.</span>
+    All posters and communication materials are available for download using the password
+        <span  style="color:#57b8d9;font-weight: bold">"OYF17"</span>.</span>
     <br/><br/>
 
     @include('shared.oyf.email_supporters')
