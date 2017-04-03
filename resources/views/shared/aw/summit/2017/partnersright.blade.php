36d325467b1ddd180702f46def32e01f3baa5be3
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-04-03 21:07:08 +0100

    aw summit pics

--- resources/views/shared/aw/summit/2017/partnersright.blade.php
+++ resources/views/shared/aw/summit/2017/partnersright.blade.php
@@ -101,3 +101,112 @@
 
 
 </div>
+
+
+@if($page=='photos')
+
+    <div class="aw-partners-coloured">
+
+        <div class="aw-partners-title">PLATINUM PARTNERS</div>
+
+        <div class="aw-partners-gold">
+
+            <p>
+                <a href="http://www.sit-stand.com" target="_blank">
+                    <img src="{{ url('/') }}/img/aw/summit/partners/sitstand.png" width="150px" height="70px" >
+                </a>
+            </p>
+
+        </div>
+
+
+
+        <div class="aw-partners-title">GOLD PARTNERS</div>
+
+        <br/>
+
+        <div class="aw-partners-gold">
+            <p>
+                <a href="https://sit-stand.com/23_yo-yo-desk" target="_blank">
+                    <img src="{{ url('/') }}/img/aw/summit/partners/yo-yo-new-logo.jpg">
+                    <!--
+                <img src="{{ url('/') }}/img/aw/summit/partners/yo-yo1.jpg">
+                -->
+                </a>
+            </p>
+
+        </div>
+
+
+
+
+
+        <div class="aw-partners-title">SILVER PARTNERS</div>
+
+        <div class="aw-partners-silver">
+
+            <p>
+                <a href="http://www.aeris.de/en/" target="_blank">
+                    <img src="{{ url('/') }}/img/aw/summit/partners/aeris.png" >
+                </a>
+            </p>
+
+            <p>
+                <a href="http://imprintmats.com/" target="_blank">
+                    <img src="{{ url('/') }}/img/aw/summit/partners/imprint.png">
+                </a>
+            </p>
+
+
+
+            <p>
+                <a href="http://www.fellowes.com/gb/en/Pages/default.aspx" target="_blank">
+                    <img src="{{ url('/') }}/img/aw/summit/partners/fellowes.jpg">
+                </a>
+            </p>
+
+            <p>
+                <a href="https://steppie.dk/en/" target="_blank">
+                    <img src="{{ url('/') }}/img/aw/summit/partners/steppie.jpg">
+                </a>
+            </p>
+
+            <p>
+                <a href="http://www.littlenudge.com/" target="_blank">
+                    <img src="{{ url('/') }}/img/aw/summit/partners/little_nudge.jpg">
+                </a>
+            </p>
+
+
+            <p style="margin-top:15px">
+                <a href="http://ergospace.se/" target="_blank">
+                    <img src="{{ url('/') }}/img/aw/summit/partners/ergo.jpg">
+                </a>
+            </p>
+
+            <p >
+                <a href="http://fitroomexpress.com/" target="_blank">
+                    <img src="{{ url('/') }}/img/aw/summit/partners/fitroom.jpg">
+                </a>
+            </p>
+
+            <p >
+                <a href="http://www.thefreedesk.com/start/" target="_blank">
+                    <img src="{{ url('/') }}/img/aw/summit/partners/freedesk.jpg">
+                </a>
+            </p>
+
+        </div>
+
+
+        <div class="aw-partners-platinum link-line">
+            <br/>
+            <p><center><a href="contact" class="aw-partners-linkr">Partnership Enquiries</a></center></p>
+            <br/>
+        </div>
+
+
+
+    </div>
+
+@endif
