66f81f8085f38cd9b8283e40b5616071debfc7bf
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-04-04 18:59:19 +0100

    aw summit update

--- resources/views/aw/summit/2017/programme-pdf.blade.php
+++ resources/views/aw/summit/2017/programme-pdf.blade.php
@@ -62,15 +62,15 @@
                 <ul>
                     <li><strong>A New Horizon - Impact of the Expert Statement</strong>
                         <br/>
-                        <a href="pdf/slides/2.Gavin_Bradley.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
-                        <!-- <a href="pdf/slides/.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp; -->
+                        <a href="pdf/slides/2.Gavin_Bradley.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
+                        <!-- <a href="pdf/slides/.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp; -->
                         <span class="aw-redtext"><i><strong>Gavin Bradley</strong>, Founding Director, Active Working CIC</span></i>
                     </li>
 
 
                     <li><strong>Sedentary Behaviour Public Health Research:  What We Now Know. What We Don’t Know Yet.</strong>
                         <br/>
-                        <a href="pdf/slides/3.Jason_Gill.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/3.Jason_Gill.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Dr Jason Gill</strong>, BHF Glasgow Cardiovascular Research Centre, University of Glasgow </span></i>
                     </li>
 
@@ -78,7 +78,7 @@
 
                     <li><strong>How Much Sitting time is Acceptable?  And How Much is Too Much?</strong>
                         <br/>
-                        <a href="pdf/slides/4.Stacy_Clemes.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/4.Stacy_Clemes.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Dr Stacy Clemes</strong>, Senior Lecturer in Human Biology	, Loughborough University</span></i>
                     </li>
 
@@ -102,13 +102,13 @@
 
                 <ul>
                     <li><strong>Workplace Evidence:  What We Now Know. What We Don’t Know Yet.</strong><br/>
-                        <a href="pdf/slides/5.Veerle_Hermans1.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/5.Veerle_Hermans1.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Dr Veerle Hermans</strong>, Head of Ergonomics (IDEWE), Prof. Ergonomics (Free University of Brussels)</span></i>
                     </li>
 
 
                     <li><strong>Behaviour Change Techniques in the Workplace</strong><br/>
-                        <a href="pdf/slides/6.Raluca_Matei.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/6.Raluca_Matei.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Raluca Matei</strong>, Research Manager (Active Working) / Psychology PhD Candidate, Manchester Metropolitan University</span></i>
                         <br/>
                     </li>
@@ -116,20 +116,20 @@
 
                     <li><strong>Health at Work:  Growing Impact of Musculoskeletal Disorders on Productivity.</strong>
                         <br/>
-                        <a href="pdf/slides/7.Stephen_Bevan.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/7.Stephen_Bevan.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Prof. Stephen Bevan</strong>, Director, The Work Foundation</span></i>
                     </li>
 
 
                     <li><strong>UK Workplace Research and Impacts on Reduction in Sedentary Behaviour.</strong>
                         <br/>
-                        <a href="pdf/slides/8.Fehmidah_Munir.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/8.Fehmidah_Munir.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Dr Fehmidah Munir</strong>, Reader in Health Psychology, Loughborough University</span></i>
                     </li>
 
                     <li><strong>Workplace Team Sport – Systematic review of individual, Group and Organisational Benefits</strong>
                         <br/>
-                        <a href="pdf/slides/9.Hilary_McDermott.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/9.Hilary_McDermott.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Hilary Mc Dermott</strong>, Senior Lecturer in Psychology, Loughborough University</span></i>
                     </li>
 
@@ -168,14 +168,14 @@
 
                     <li><strong>Ergonomics of Sit-Stand Solutions.</strong>
                         <br/>
-                        <a href="pdf/slides/10.Veerle_Hermans2.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/10.Veerle_Hermans2.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Dr Veerle Hermans</strong>, Head of Ergonomics (IDEWE), Prof. Ergonomics (Free University of Brussels)</span></i>
                     </li>
 
 
                     <li><strong>Tools to Identify, Diagnose and Remediate Health Risks of Sedentary Behaviour in the Office</strong>
                         <br/>
-                        <a href="pdf/slides/11.Little_Nudge.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/11.Little_Nudge.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Emily Tims</strong>, Managing Director, Little Nudge</span></i>
                         <!--
                         <br/>
@@ -186,7 +186,7 @@
 
                     <li><strong>Creating an Open Culture to Promote Employee Wellbeing Awareness and Action</strong>
                         <br/>
-                        <a href="pdf/slides/12.Amy_McKeown-HealthEY.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/12.Amy_McKeown-HealthEY.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Amy Mc Keown</strong>, Health EY, People Advisory Services</span></i>
 
                     </li>
@@ -194,7 +194,7 @@
 
                     <li><strong>Education, Education, Education.</strong>
                         <br/>
-                        <a href="pdf/slides/14. Sue_Jutley-Active-Working.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/14. Sue_Jutley-Active-Working.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Sue Jutley</strong>, Head of Engagement, Active Working Group</span></i>
                     </li>
 
@@ -202,7 +202,7 @@
 
                     <li><strong>Breaking Down the Barriers to Equipment and Education.</strong>
                         <br/>
-                        <a href="pdf/slides/13.Sit-Stand.Com.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/13.Sit-Stand.Com.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Luke Armstrong</strong>, Head of Business Development, Sit-Stand.Com</span></i>
                     </li>
 
@@ -213,7 +213,7 @@
 
                     <li><strong>Corporate Gyms: Strategies to Increase Employee Usage and Engagement.</strong>
                         <br/>
-                        <a href="pdf/slides/15.James_Shillaker-GymFlex.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/15.James_Shillaker-GymFlex.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>James Shillaker</strong>, CEO, Incorpore</span></i>
                     </li>
 
@@ -232,14 +232,14 @@
                 <ul>
                     <li><strong>Cost / Benefit Analysis for Active Working and Bringing about Behaviour Change.</strong>
                         <br/>
-                        <a href="pdf/slides/16.William_Bird-Intelligent-Health.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/16.William_Bird-Intelligent-Health.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Dr William Bird</strong>, CEO, Intelligent Health </span></i>
                     </li>
 
 
                     <li><strong>Breaking the Mould. The Challenge for Workplace Design.</strong>
                         <br/>
-                        <a href="pdf/slides/17.Sam_Sahni-Morgan-Lovell.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/17.Sam_Sahni-Morgan-Lovell.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Sam Sahni</strong>, Head of Workplace Consultancy, Morgan Lovell</span></i>
                     </li>
 
@@ -247,10 +247,10 @@
 
                     <li><strong>Our Journey to Active Working: Company Case Studies.</strong>
                         <br/>
-                        <a href="pdf/slides/18.Case_Study-Austin-Fraser.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/18.Case_Study-Austin-Fraser.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Laura Johnstone</strong>, HR Director, Austin Fraser</span></i>
                         <br/>
-                        <a href="pdf/slides/19.Case_Study-Macmillan.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <a href="pdf/slides/19.Case_Study-Macmillan.pdf" target="_blank"><img src="{{ url('/') }}/img/general/icons/pdficon.png" width="20"></a>&nbsp;
                         <span class="aw-redtext"><i><strong>Jo Foster</strong>, Physical Activity Programme Lead, Macmillan Cancer Support</span></i>
                         <br/>
                         <span class="aw-redtext"><i><strong>Kevin Tesh</strong>, Principal Ergonomist, Johnson & Johnson Medical </span></i>
