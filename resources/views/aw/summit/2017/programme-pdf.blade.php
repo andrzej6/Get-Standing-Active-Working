25fe0faf431eab9cdee382211fe396ddd9a1f0a0
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-04-04 18:42:21 +0100

    aw summit update

---
+++ resources/views/aw/summit/2017/programme-pdf.blade.php
@@ -0,0 +1,334 @@
+@extends('layouts.aw.summit-2017')
+
+@section('header-styles')
+    @parent
+@endsection
+
+
+@section('description')
+    Active Working provide thought leadership to reduce prolonged sitting in the workplace. Working with government,
+    medical science, health charities and employers.
+@endsection
+
+@section('keywords')
+    active-working, productive-working, office-productivity, sit-stand, standing-desk, stand-up desk, sit-n-stand,
+    wellness, office-wellness, employee-wellness, sedentary-office, wellbeing, physical-activity, employee-engagement,
+    staff-resilience, stress-management, productivity, occupational-health, height-adjustable-desk, healthy-office, health-and-safety,
+    office-wellbeing, well-being
+@endsection
+
+@section('title')
+    Active Working Summit 2017. Agenda
+@endsection
+
+
+@section('aw-maincontent')
+    <br/>
+
+    <div style="color:#3563ae">
+        <div style="font-size:22px;color:#3563ae;margin-bottom:5px"><strong>By Dame Carol Black</strong> (Event Ambassador)</div>
+        <span style="font-style: italic">
+            Expert Adviser on Health and Work to <strong>NHS England and Public Health England</strong>.
+        </span>
+    </div>
+
+    <br/>
+
+    <div style="font-style: italic">
+        "Consistent evidence has shown the health benefits of physical activity.  We also know about the harms resulting
+        from the lack of activity, from living a sedentary life.  Being active is good for us, not just
+        physically – that is obvious – but also for our state of mind, our mental health, and our overall sense of wellbeing.<br/><br/>
+
+        Being inactive increases the risk of dying early.  It is worse than smoking.  Fortunately it seems that
+        moderate physical activity of about an hour a day can remove the increased risk of death associated with
+        prolonged sitting time at work.  So there are grounds for adding physical activity to the sedentary life
+        in the workplace, where many people are occupied much of the day sitting at a desk.<br/><br/>
+
+        I am pleased that the Active Working Summit will bring this evidence to wider notice, encouraging changes
+        in working practice that will help promote and safeguard the health and wellbeing of many people."
+
+        <br/><br/>
+        <img src="{{ url('/') }}/img/summit/gb/2017/brochurepic3.jpg" >
+
+    </div>
+
+    <br/><br/>
+
+    <div style="text-align: center">
+
+        <div class="">
+
+            <div style="font-weight: bold;color:red;">
+                Book <span style="text-decoration: underline;font-weight: bold;">before 30th April 2016</span> and receive a
+                <span style="text-decoration: underline;font-weight: bold;">30% discount </span> to Active Working Summit 2017.<br/><br/>
+
+            </div>
+
+            <div class="aw-support-statement">
+                The Active Working Summit is officially supported by Dame Sally Davies, Chief Medical Officer of England.
+
+            </div>
+            <br/>
+            The programme for 2018 will be published in November 2017.
+
+            <br/><br/>
+            Our last event included the following sessions and speakers:<br/><br/>
+
+
+
+            <div class="aw-redtext bigger"><strong>M O D E R A T O R</strong></div>
+
+            <div class="aw-redtext">
+                <i><strong>Julian Worricker</strong>, Broadcaster & Journalist,  BBC</i>
+
+            </div>
+
+            <br/>
+
+            <div class="aw-redtext bigger"><strong>K E Y N O T E&nbsp;&nbsp;&nbsp;S  P E E C H</strong></div>
+
+            <div class="aw-redtext">
+                <a href="pdf/slides/1.James_Levine.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                <i><strong>James Levine</strong>, Professor of Medicine, Director Obesity Solutions, Mayo Clinic and Arizona State University</i>
+
+            </div>
+
+
+            <br/>
+
+            <div class="summit-section">
+
+                <div class="section-bold"><span class="">S E S S I O N 1: </span>
+                    Sedentary Behaviour:  Health Research Evidence and Key Priorities</div>
+
+                <ul>
+                    <li><strong>A New Horizon - Impact of the Expert Statement</strong>
+                        <br/>
+                        <a href="pdf/slides/2.Gavin_Bradley.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <!-- <a href="pdf/slides/.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp; -->
+                        <span class="aw-redtext"><i><strong>Gavin Bradley</strong>, Founding Director, Active Working CIC</span></i>
+                    </li>
+
+
+                    <li><strong>Sedentary Behaviour Public Health Research:  What We Now Know. What We Don’t Know Yet.</strong>
+                        <br/>
+                        <a href="pdf/slides/3.Jason_Gill.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Dr Jason Gill</strong>, BHF Glasgow Cardiovascular Research Centre, University of Glasgow </span></i>
+                    </li>
+
+
+
+                    <li><strong>How Much Sitting time is Acceptable?  And How Much is Too Much?</strong>
+                        <br/>
+                        <a href="pdf/slides/4.Stacy_Clemes.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Dr Stacy Clemes</strong>, Senior Lecturer in Human Biology	, Loughborough University</span></i>
+                    </li>
+
+
+
+
+
+
+
+                </ul>
+
+            </div>
+
+
+
+            <div class="summit-section">
+
+                <div class="section-bold"><span class="">S E S S I O N 2: </span>
+                    Sedentary Behaviour:  Workplace Research and Overcoming the Challenges
+                </div>
+
+                <ul>
+                    <li><strong>Workplace Evidence:  What We Now Know. What We Don’t Know Yet.</strong><br/>
+                        <a href="pdf/slides/5.Veerle_Hermans1.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Dr Veerle Hermans</strong>, Head of Ergonomics (IDEWE), Prof. Ergonomics (Free University of Brussels)</span></i>
+                    </li>
+
+
+                    <li><strong>Behaviour Change Techniques in the Workplace</strong><br/>
+                        <a href="pdf/slides/6.Raluca_Matei.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Raluca Matei</strong>, Research Manager (Active Working) / Psychology PhD Candidate, Manchester Metropolitan University</span></i>
+                        <br/>
+                    </li>
+
+
+                    <li><strong>Health at Work:  Growing Impact of Musculoskeletal Disorders on Productivity.</strong>
+                        <br/>
+                        <a href="pdf/slides/7.Stephen_Bevan.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Prof. Stephen Bevan</strong>, Director, The Work Foundation</span></i>
+                    </li>
+
+
+                    <li><strong>UK Workplace Research and Impacts on Reduction in Sedentary Behaviour.</strong>
+                        <br/>
+                        <a href="pdf/slides/8.Fehmidah_Munir.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Dr Fehmidah Munir</strong>, Reader in Health Psychology, Loughborough University</span></i>
+                    </li>
+
+                    <li><strong>Workplace Team Sport – Systematic review of individual, Group and Organisational Benefits</strong>
+                        <br/>
+                        <a href="pdf/slides/9.Hilary_McDermott.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Hilary Mc Dermott</strong>, Senior Lecturer in Psychology, Loughborough University</span></i>
+                    </li>
+
+                    <li><strong>Key Stakeholders:  Dealing with the Challenges of Sedentary Working (Panel Discussion).</strong>
+                        <br/>
+                        <!--
+                        <span style="color:#da3b44;"><strong>2 minute introduction from each Stakeholder rep, which will
+                            also include a “brief opening statement”.</strong></span><br/>-->
+
+
+                        <span class="aw-redtext"><i><strong>Mike Knapton</strong>, Associate Medical Director, 	British Heart Foundation</span></i>
+                        <br/>
+                        <span class="aw-redtext"><i><strong>Jo Foster</strong>, Physical Activity Lead, Macmillan </span></i>
+                        <br/>
+                        <span class="aw-redtext"><i><strong>Stuart Blackman</strong>, Chief Executive, BackCare</span></i>
+                        <br/>
+                        <span class="aw-redtext"><i><strong>Ben Wilmott</strong>, Head of Strategy, CIPD</span></i>
+
+
+                    </li>
+
+                </ul>
+
+            </div>
+
+
+
+
+            <div class="summit-section">
+
+                <div class="section-bold"><span class="">S E S S I O N 3: </span>
+                    Solutions Promoting Active Working
+                </div>
+
+                <ul>
+
+                    <li><strong>Ergonomics of Sit-Stand Solutions.</strong>
+                        <br/>
+                        <a href="pdf/slides/10.Veerle_Hermans2.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Dr Veerle Hermans</strong>, Head of Ergonomics (IDEWE), Prof. Ergonomics (Free University of Brussels)</span></i>
+                    </li>
+
+
+                    <li><strong>Tools to Identify, Diagnose and Remediate Health Risks of Sedentary Behaviour in the Office</strong>
+                        <br/>
+                        <a href="pdf/slides/11.Little_Nudge.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Emily Tims</strong>, Managing Director, Little Nudge</span></i>
+                        <!--
+                        <br/>
+                        <span class="aw-redtext"><i><strong>Roy Cochran</strong>, Lead, Ergonomist & Health and Safety Lead, BUPA</span></i>
+                        -->
+                    </li>
+
+
+                    <li><strong>Creating an Open Culture to Promote Employee Wellbeing Awareness and Action</strong>
+                        <br/>
+                        <a href="pdf/slides/12.Amy_McKeown-HealthEY.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Amy Mc Keown</strong>, Health EY, People Advisory Services</span></i>
+
+                    </li>
+
+
+                    <li><strong>Education, Education, Education.</strong>
+                        <br/>
+                        <a href="pdf/slides/14. Sue_Jutley-Active-Working.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Sue Jutley</strong>, Head of Engagement, Active Working Group</span></i>
+                    </li>
+
+
+
+                    <li><strong>Breaking Down the Barriers to Equipment and Education.</strong>
+                        <br/>
+                        <a href="pdf/slides/13.Sit-Stand.Com.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Luke Armstrong</strong>, Head of Business Development, Sit-Stand.Com</span></i>
+                    </li>
+
+
+
+
+
+
+                    <li><strong>Corporate Gyms: Strategies to Increase Employee Usage and Engagement.</strong>
+                        <br/>
+                        <a href="pdf/slides/15.James_Shillaker-GymFlex.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>James Shillaker</strong>, CEO, Incorpore</span></i>
+                    </li>
+
+                </ul>
+
+            </div>
+
+
+
+            <div class="summit-section">
+
+                <div class="section-bold"><span class="">S E S S I O N 4: </span>
+                    Company Case Studies & the Business Case for Active Working
+                </div>
+
+                <ul>
+                    <li><strong>Cost / Benefit Analysis for Active Working and Bringing about Behaviour Change.</strong>
+                        <br/>
+                        <a href="pdf/slides/16.William_Bird-Intelligent-Health.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Dr William Bird</strong>, CEO, Intelligent Health </span></i>
+                    </li>
+
+
+                    <li><strong>Breaking the Mould. The Challenge for Workplace Design.</strong>
+                        <br/>
+                        <a href="pdf/slides/17.Sam_Sahni-Morgan-Lovell.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Sam Sahni</strong>, Head of Workplace Consultancy, Morgan Lovell</span></i>
+                    </li>
+
+
+
+                    <li><strong>Our Journey to Active Working: Company Case Studies.</strong>
+                        <br/>
+                        <a href="pdf/slides/18.Case_Study-Austin-Fraser.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Laura Johnstone</strong>, HR Director, Austin Fraser</span></i>
+                        <br/>
+                        <a href="pdf/slides/19.Case_Study-Macmillan.pdf" target="_blank"><img src="/images/icon/pdficon.png" width="20"></a>&nbsp;
+                        <span class="aw-redtext"><i><strong>Jo Foster</strong>, Physical Activity Programme Lead, Macmillan Cancer Support</span></i>
+                        <br/>
+                        <span class="aw-redtext"><i><strong>Kevin Tesh</strong>, Principal Ergonomist, Johnson & Johnson Medical </span></i>
+
+                    </li>
+
+
+
+                    <li><strong>Panel Discussion.</strong>
+                        <br/>
+                        <span class="aw-redtext"><i><strong></strong></span></i>
+                    </li>
+
+                </ul>
+
+            </div>
+
+
+
+            <div class="aw-redtext bigger"><strong>C L O S I N G&nbsp;&nbsp;&nbsp;A D D R E S S</strong></div>
+
+            <strong>“Blueprint for an Active Britain. The Workplace”</strong>
+
+            <div class="aw-redtext">
+                <i><strong>Baroness Tanni Grey-Thompson</strong>, Chair, ukactive</i>
+
+            </div>
+
+            <br/><br/>
+            <div class="aw-redtext bigger">N E T W O R K I N G &nbsp;&nbsp;&nbsp;  D R I N K S</div>
+
+
+
+        </div>
+
+
+    </div>
+
+@endsection
