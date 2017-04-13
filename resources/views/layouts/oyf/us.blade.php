71a260871bd7985ffaaf191e5281694def16c58d
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-04-13 12:40:47 +0100

    oyf slider+docs update

--- resources/views/layouts/oyf/us.blade.php
+++ resources/views/layouts/oyf/us.blade.php
@@ -22,6 +22,8 @@ $sidemenu=array('index'=>'Event','downloads'=>'Downloads');
         @include('shared.oyf.us.partnersleft')
         <hr class="light" style="height:0;">
         @include('shared.oyf.us.partnersleft')
+        <hr class="light" style="height:0;">
+        @include('shared.oyf.us.partnersleft')
     @endif
 
 
