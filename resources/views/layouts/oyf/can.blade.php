a76179431b3007362e06c27b1f07511dffce5c84
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-04-13 12:57:45 +0100

    oyf slider+docs update

--- resources/views/layouts/oyf/can.blade.php
+++ resources/views/layouts/oyf/can.blade.php
@@ -22,6 +22,12 @@ $sidemenu=array('index'=>'Event','downloads'=>'Downloads');
         @include('shared.oyf.can.partnersleft')
         <hr class="light" style="height:0;">
         @include('shared.oyf.can.partnersleft')
+        <hr class="light" style="height:0;">
+        @include('shared.oyf.can.partnersleft')
+        <hr class="light" style="height:0;">
+        @include('shared.oyf.can.partnersleft')
+        <hr class="light" style="height:0;">
+        @include('shared.oyf.can.partnersleft')
     @endif
 @endsection
 
