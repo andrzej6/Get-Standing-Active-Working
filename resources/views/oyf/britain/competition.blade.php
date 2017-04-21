554036e747e83261ef0f7e0ca9953e23f5f5d453
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-04-21 16:41:37 +0100

    oyf newpage

--- resources/views/oyf/britain/competition.blade.php
+++ resources/views/oyf/britain/competition.blade.php
@@ -31,12 +31,13 @@
 
     <br/>
     To celebrate <strong>On Your Feet Day</strong>, our official sponsor
-    <a href="https://sit-stand.com/" style="color:#EA1E30;font-weight: bold;text-decoration:underline" target="_blank">Sit-Stand.Com</a> is kindly
+    <a href="https://sit-stand.com/" style="color:#ff3f72;font-weight: bold;text-decoration:underline" target="_blank">Sit-Stand.Com</a> is kindly
     offering the 5 free Yo-YoDesks®  as prizes for the best "Get up offa that thing" PHOTOS of you and your co-workers!
     What will you do?<br/><br/>
 
-    To enter simply post your photos on <span style="color:#EA1E30;font-weight: bold">Facebook</span> or
-    <span style="color:#EA1E30;font-weight: bold">Twitter</span> using the
+    To enter simply post your photos on
+    <a href="https://www.facebook.com/onyourfeet.britain/" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Facebook</a> or
+    <a href="https://twitter.com/getGBstanding" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Twitter</a> using the
     <span style="color:#57b8d9;font-weight: bold">#OYF17</span> or
     <span style="color:#57b8d9;font-weight: bold">#OnYourFeetBritain</span> and
     <a href="https://twitter.com/getGBstanding?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">@getGBstanding</a>
@@ -45,9 +46,12 @@
     <br/><br/><br/>
     <span style="color:#57b8d9;font-weight: bold;font-size: 16px">Prizes for best photos:</span>
     <br/><br/>
-    <a href="https://sit-stand.com/desk-risers/120-yo-yo-desk-mini.html"><img src="https://sit-stand.com/1382-atmn_small/yo-yo-desk-mini.jpg"></a>
-    <a href="https://sit-stand.com/desk-risers/120-yo-yo-desk-mini.html"><img src="https://sit-stand.com/1384-atmn_small/yo-yo-desk-mini.jpg"></a>
-    <a href="https://sit-stand.com/desk-risers/120-yo-yo-desk-mini.html"><img src="https://sit-stand.com/1394-atmn_small/yo-yo-desk-mini.jpg"></a>
+    <a href="https://sit-stand.com/desk-risers/120-yo-yo-desk-mini.html" target="_blank">
+        <img src="https://sit-stand.com/1382-atmn_normal/yo-yo-desk-mini.jpg" width="150"></a>&nbsp;&nbsp;
+    <a href="https://sit-stand.com/desk-risers/120-yo-yo-desk-mini.html" target="_blank">
+        <img src="https://sit-stand.com/1384-atmn_normal/yo-yo-desk-mini.jpg" width="150"></a>&nbsp;&nbsp;
+    <a href="https://sit-stand.com/desk-risers/120-yo-yo-desk-mini.html" target="_blank">
+        <img src="https://sit-stand.com/1394-atmn_normal/yo-yo-desk-mini.jpg" width="150"></a>
     <br/><br/>
 
     <div style="display: inline-block;width:100px">Best Group:</div>
