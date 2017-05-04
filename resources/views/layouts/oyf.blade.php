25eb2bdb813d1149d7f9d88734de2c28626ec717
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-05-04 21:47:35 +0100

    aw css corrections

--- resources/views/layouts/oyf.blade.php
+++ resources/views/layouts/oyf.blade.php
@@ -72,11 +72,13 @@
 
 
 
+                                    @if ($country != 'signup')
                                     <div class="aw-button-cont">
                                         <a class="aw-menu-button" id="aw-menub" href="">
                                             <img src="{{ url('/') }}/img/general/toggle-button.png"/> &nbsp; <strong>MENU</strong>
                                         </a>
                                     </div>
+                                    @endif
 
 
 
