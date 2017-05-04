2a6659d1db69a3da36e0f79e76305b2672fb30f1
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-05-04 16:11:38 +0100

    aw css corrections

--- resources/views/aw/innovation_center.blade.php
+++ resources/views/aw/innovation_center.blade.php
@@ -68,7 +68,7 @@
 
 @section('content-restofsmall')
     <a href="http://onyourfeetday.com/britain" class="darken" target="_blank">
-        <img src="{{ url('/') }}/img/getstanding/gb/ads/oyf/oyf3.jpg" alt="oyf"/>
+        <img src="{{ url('/') }}/img/getstanding/gb/ads/oyf/oyf18.jpg" alt="oyf"/>
     </a>
 
     <a href="careers" target="_blank">
