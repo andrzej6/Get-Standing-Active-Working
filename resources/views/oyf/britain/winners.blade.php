cd47a342cf64f4e300285527cdda4eeaf9c61c78
andrzej dlugosz <andrzejdlugosz@yahoo.co.uk>
2017-05-26 11:03:36 +0100

    oyf winners blog

---
+++ resources/views/oyf/britain/winners.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.oyf.gb')
+
+@section('header-styles')
+    @parent
+    <link rel="stylesheet" type="text/css" href="{{ url('/') }}/css/oyf/mix.css">
+@endsection
+
+@section('description')
+    On Your Feet. Regular minor movement whilst at work is essential for us to
+    keep our bodies healthy, prevent illness and relieve stress. Sit-Stand workstations
+    enabling us to mix it up between periods of sitting with standing.
+@endsection
+
+@section('keywords')
+    on-your-feet, active-working, productive-working, office-productivity, sit-stand,
+    sit-stand-desk, standing-desk, height-adjustable-desk,
+    variable-height-desk, healthy-office, treadmill-desk, desk-riser, desk-mount, home-office,
+    health-and-safety, wellness
+@endsection
+
+@section('title')
+    On Your Feet Britain. Winners Blog.
+@endsection
+
+
+
+@section('aw-maincontent')
+
+    <h1 class="oyf-newpink">Photo Competition | Winners</h1>
+
+
+    <span style="font-size:14px">Active workers making sure #SitLess #MoveMore message goes beyond On Your Feet Day.</span>
+
+
+    On Your Feet Day (OYF) is now a standing fixture (sorry!) on the workplace wellbeing calendar, and this year’s event saw a record number of businesses and employees adopting the ‘wiggle it waggle it’ message to boost health.
+    More than 2,500 businesses and 1.5 million office workers signed up for OYF 2017 – organised by Get Britain Standing to raise awareness of the dangers of sedentary behaviour in the workplace. And it’s not just UK workers taking part: since it started back in 2015, On Your Feet Day has now been adopted by countries as widespread as Spain, USA, Canada, Ireland and Australia.
+    To mark this year’s OYF Day, sponsor Sit-Stand.Com kindly donated some Yo-Yo Desks as prizes for star participants in the event. Many thanks to all who entered: we LOVED seeing all your fun, imaginative and instructive images and videos.
+    Our winners have kindly shared their inspiration below… and are now fully enjoying the #SitLess #MoveMore workplace vibe with their Yo-Yo Desk MINI®.
+    Visit Get Britain Standing for information about the benefits of getting active during your working day
+
+    <br/><br/>
+
+    Visit Sit-Stand.Com to find out more about their stylish, affordable range of standing desks and desk risers
+
+
+
+    <br/><br/>
+    <video width="400" height="300" controls>
+        <source src="{{ url('/') }}/img/video/oyf/firefighters.mp4" type="video/mp4">
+        <source src="{{ url('/') }}/img/video/oyf/firefighters.mp4" type="video/ogg">
+        Your browser does not support the video tag.
+    </video>
+
+    <br/><br/>
+
+    Winners Story | Best Male | ITEC Skills
+    “Before On Your Feet (OYF) day, the majority of our staff, other than our winner Tom, spent the day working away at their desks. However, Tom’s always had a very “stand-up” approach to the working day. Tom feels more energetic and confident when he stands up on the phone, but stretching the phone and bending down to reach the computer started to become a bit of a pain for him.
+    The sales and marketing team really enjoyed OYF as it was a chance to get moving ready for the weekend ahead, Tom actually ripped his work trousers while testing out some of his moves (luckily you can’t see that in the video!). It took us a few attempts and lots of laughs to record our Boomerang video for the OYF competition but it was all worth it when we found out we’d won.
+    Since winning best male, Tom’s been the envy of the office with his new Yo-Yo Desk MINI® and he loves the fact he can stand up all day with his brand-new desk.”
+
+    <br/><br/>
+
+    Winner Story | Best Female | Purple Sprout
+    The Purple Sprout office has always been a particularly active one as the team are all interested in health and wellbeing, particularly researching new recipes and fitness regimes – a few members of the team regularly exercise at local gyms and partake in fitness boot camps (especially as we head into summer!)
+    During OYF Day 2017, we started the day by doing funny dances filmed using the Instagram filter, Boomerang, for our social media pages (the video of Tracey Thake, Purple Sprout Director, was the video that won us the competition!). As the weather was so nice that day, two members of the team also went for a walk around a nearby housing estate during their lunch hour to stretch their legs.
+    The Yo-Yo Desk MINI® is a fantastic addition to our office and is used daily by Tracey. She at least tries to stand up for the first few hours of the day – motivation wasn’t needed when we googled how many calories she could burn by standing at her desk!
+
+
+@endsection
+
