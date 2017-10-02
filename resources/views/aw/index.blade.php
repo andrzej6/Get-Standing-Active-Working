7e1e28e38ec3343f84e7d1130312891c3071d8ac
andrzej6 <andrzejdlugosz@yahoo.co.uk>
2017-10-02 09:00:11 +0100

    back to school take down

--- resources/views/aw/index.blade.php
+++ resources/views/aw/index.blade.php
@@ -317,6 +317,12 @@
                         </a>
                     </div>
 
+                     <div>
+                        <a href="https://sit-stand.com/back-to-school" target="_blank">
+                            <img src="{{ url('/') }}/img/getstanding/gb/slider/thin/go-to-school.jpg">
+                        </a>
+                    </div>
+
 
 
 
@@ -325,11 +331,7 @@
 
 
 
-                    <div>
-                        <a href="https://sit-stand.com/back-to-school" target="_blank">
-                            <img src="{{ url('/') }}/img/getstanding/gb/slider/thin/go-to-school.jpg">
-                        </a>
-                    </div>
+
 
 
                     <div>
