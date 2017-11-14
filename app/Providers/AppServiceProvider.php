8320e70224b203adc981f07c62530b47ee4490ae
andrzej6 <andrzejdlugosz@yahoo.co.uk>
2017-11-14 16:51:52 +0000

    featured media correction

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,9 +18,18 @@ class AppServiceProvider extends ServiceProvider
 
 
         switch ($request->server("HTTP_HOST")) {
-            case '127.0.0.1':
+            case 'getamericastanding.org':
+                $featured_art = Article::where('country', 'LIKE', '%4%')->where('featured', 'LIKE', '%4%')->orderby('date_posted', 'desc')->take(2)->get();
+                break;
+
+            case 'getcanadastanding.org':
                 $featured_art = Article::where('country', 'LIKE', '%3%')->where('featured', 'LIKE', '%3%')->orderby('date_posted', 'desc')->take(2)->get();
                 break;
+
+            case 'getaustraliastanding.org':
+                $featured_art = Article::where('country', 'LIKE', '%2%')->where('featured', 'LIKE', '%2%')->orderby('date_posted', 'desc')->take(2)->get();
+                break;
+
             default:
                 $featured_art = Article::where('country', 'LIKE', '1%')->where('featured', 'LIKE', '1%')->orderby('date_posted', 'desc')->take(2)->get();
                 break;
