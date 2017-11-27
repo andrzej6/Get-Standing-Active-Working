9df9b20132a3e2834b7a8425f6d2d4189cb16d3a
sst-support <andrzejdlugosz678@gmail.com>
2017-11-27 03:39:30 +0000

    black friday takedown

--- resources/views/aw/index.blade.php
+++ resources/views/aw/index.blade.php
@@ -327,16 +327,17 @@
                         </a>
                     </div>
 
+<div>
+					<a href="https://sit-stand.com/black-friday" target="_blank">
+						<img src="{{ url('/') }}/img/getstanding/gb/slider/thin/bf-homepage.jpg">
+					</a>
+				</div>
 
 
 
                 -->
 				
-				<div>
-					<a href="https://sit-stand.com/black-friday" target="_blank">
-						<img src="{{ url('/') }}/img/getstanding/gb/slider/thin/bf-homepage.jpg">
-					</a>
-				</div>
+
 
 
                     <div>
