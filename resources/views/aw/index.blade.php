e87c81374fea3649df0844e0f6514540b61234ae
andrzej6 <andrzejdlugosz@yahoo.co.uk>
2018-01-18 13:28:52 +0000

    aw summit 2019 changes - to test6

--- resources/views/aw/index.blade.php
+++ resources/views/aw/index.blade.php
@@ -43,27 +43,34 @@
 
                     <div class="module_content slider_type1">
                         <div id="banner1" class="OneByOne_container">
+						
+						
+						<div class="oneByOne_item ls_bg1">
+                                <img src="{{ url('/') }}/img/getstanding/gb/slider/big/oyf-thankyou.jpg" alt="oyf" class="go-to go-to-onyourfeet">
+                            </div>
+						
 
+						<div class="oneByOne_item ls_bg1">
+                                <img src="{{ url('/') }}/img/getstanding/gb/slider/big/summit19.jpg" alt="oyf" class="go-to go-to-book">
+                            </div>
+							
+							<div class="oneByOne_item ls_bg1">
+                                <img src="{{ url('/') }}/img/getstanding/gb/slider/big/slider4.png" alt="weight loss" class="go-to go-to-statement">
+                            </div>
+						
+						
                             <div class="oneByOne_item ls_bg1">
                                 <img src="{{ url('/') }}/img/getstanding/gb/slider/big/our_vision.jpg" alt="oyf" class="go-to go-to-careers">
                             </div>
 
-
-
-                            <div class="oneByOne_item ls_bg1">
-                                <img src="{{ url('/') }}/img/getstanding/gb/slider/big/oyf-thankyou.jpg" alt="oyf" class="go-to go-to-onyourfeet">
-                            </div>
-
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 
                             <div class="oneByOne_item ls_bg1">
                                 <img src="{{ url('/') }}/img/getstanding/gb/slider/big/survey-home1.jpg" alt="oyf" class="go-to go-to-survey">
                             </div>
 
 
-                            <div class="oneByOne_item ls_bg1">
-                                <img src="{{ url('/') }}/img/getstanding/gb/slider/big/slider4.png" alt="weight loss" class="go-to go-to-statement">
-                            </div>
+                            
 
 
 
@@ -649,6 +656,9 @@
             window.open('http://activeworking.com/pdfs/survey_results.pdf','_blank');
         });
 
+		$(".go-to-book").click(function(event){
+            window.open('http://activeworking.com/summit/2017/book','_blank');
+        });
 
     </script>
 @endsection
