f6690579ef81c18f425050e1d8827c9b9db822a2
sst-support <andrzejdlugosz678@gmail.com>
2018-01-23 17:20:17 +0000

    aw youtube social footer link

--- resources/views/shared/aw/footer.blade.php
+++ resources/views/shared/aw/footer.blade.php
@@ -18,8 +18,9 @@
 
             <ul class="footer_socials main_socials">
 
-                <li><a href="https://twitter.com/ACTIVEworking" class="ico_social-twitter" title="Twitter"></a></li>
-                <li><a href="https://www.linkedin.com/company/9496295?trk=tyah&trkInfo=clickedVertical%3Acompany%2CclickedEntityId%3A9496295%2Cidx%3A1-1-1%2CtarId%3A1439890418404%2Ctas%3Aactive%20working" class="ico_social-flickr" title="Linked In"></a></li>
+                <li><a href="https://twitter.com/ACTIVEworking" class="ico_social-twitter" title="Twitter" target="_blank"></a></li>
+                <li><a href="https://www.linkedin.com/company/9496295?trk=tyah&trkInfo=clickedVertical%3Acompany%2CclickedEntityId%3A9496295%2Cidx%3A1-1-1%2CtarId%3A1439890418404%2Ctas%3Aactive%20working" class="ico_social-flickr" title="Linked In" target="_blank"></a></li>
+				<li><a href="https://www.youtube.com/channel/UCJFqihtZ_9dSYy_yu2s7lgA" class="ico_social-youtube" title="You Tube" target="_blank"></a></li>
 
             </ul>
 
