f121f8c5276ec33fb33097c2449f75e96fb6128d
andrzej6 <andrzejdlugosz@yahoo.co.uk>
2018-02-09 13:25:15 +0000

    oyf page all -ready

--- resources/views/layouts/oyf/eu.blade.php
+++ resources/views/layouts/oyf/eu.blade.php
@@ -23,7 +23,20 @@ $sidemenu=array('index'=>'Event','downloads'=>'Downloads');
             <div id="banner1" class="OneByOne_container">
 
                 <div class="oneByOne_item ls_bg1">
-                    <img src="{{ url('/') }}/img/oyf/eu/slider/oyf-eu18.jpg" alt="" class="go-to-onyourfeet-r">
+                    <img src="{{ url('/') }}/img/oyf/eu/slider/oyf18-sally.jpg" alt="" class="go-to-onyourfeet-r">
+                </div>
+				
+				<div class="oneByOne_item ls_bg1">
+                    <img src="{{ url('/') }}/img/oyf/eu/slider/oyf18-josh.jpg" alt="" class="go-to-onyourfeet-r">
+                </div>
+				
+				
+				<div class="oneByOne_item ls_bg1">
+                    <img src="{{ url('/') }}/img/oyf/eu/slider/oyf18-andy.jpg" alt="" class="go-to-onyourfeet-r">
+                </div>
+				
+				<div class="oneByOne_item ls_bg1">
+                    <img src="{{ url('/') }}/img/oyf/eu/slider/oyf18-amy.jpg" alt="" class="go-to-onyourfeet-r">
                 </div>
 
 
