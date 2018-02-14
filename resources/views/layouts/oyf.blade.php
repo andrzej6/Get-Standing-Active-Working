0ab45ed9cf0b68df4b397a005e6eefcc39703cf7
sst-support <andrzejdlugosz678@gmail.com>
2018-02-14 21:05:19 +0000

    oyf analytics tracking

--- resources/views/layouts/oyf.blade.php
+++ resources/views/layouts/oyf.blade.php
@@ -169,6 +169,17 @@
 
 <script type="text/javascript" src="{{ url('/') }}/js/oyf/main.js"></script>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-52391297-9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-52391297-9');
+</script>
+
+
 @yield('footer-scripts')
 
 </body>
