47dfdbf552107e2854bda7e3275438e8e4bc077f
andrzej6 <andrzejdlugosz@yahoo.co.uk>
2018-02-15 08:56:10 +0000

    js aw tracking

--- public/js/aw/track.js
+++ public/js/aw/track.js
@@ -1,17 +1,9 @@
- var _gaq = _gaq || [];
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-52391297-7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  _gaq.push(['_setAccount', 'UA-52391297-1']);
-
-  _gaq.push(['_trackPageview']);
-
-
-
-  (function() {
-
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-
-  })();
+  gtag('config', 'UA-52391297-7');
+</script>
