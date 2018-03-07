95672877d002f8f4afb733dd60d512b5cec1a807
andrzej6 <andrzejdlugosz@yahoo.co.uk>
2018-03-07 15:03:38 +0000

    oyf sponsor try4

--- resources/views/shared/oyf/us/partnersleft.blade.php
+++ resources/views/shared/oyf/us/partnersleft.blade.php
@@ -1,19 +1,55 @@
 <div class="aw-partners-strategic">
 
+<div class="aw-partners-title">PLATINUM PARTNERS</div>
+
+   <div class="aw-partners-gold">
+        <p style="margin-bottom: 0">
+            <a href="http://yo-yodesk.com/" target="_blank">
+                <img src="{{ url('/') }}/img/aw/summit/partners/yo-yo-new-logo.png" width="150px" height="70px">
+            </a>
+        </p>
+
+    </div>
+
+
+
+   
+
+
+    <div class="aw-partners-title">GOLD PARTNERS</div>
+
+   
+    <div class="aw-partners-gold">
+
+        <p style="margin-bottom: 0">
+            <a href="https://www.workathlete.com/" target="_blank">
+                <img src="{{ url('/') }}/img/aw/summit/partners/wathlete.png">
+            </a>
+        </p>
+
+    </div>
+
+	
+	
+	 <div class="aw-partners-platinum">
+        <p>
+            <a href="https://sit-stand.com/" target="_blank">
+                <img src="{{ url('/') }}/img/oyf/partners/sitstand.png" >
+            </a>
+        </p>
+    </div>
+
+<!--
     <div class="aw-partners-title">PLATINUM PARTNERS</div>
 
     <div class="aw-partners-platinum">
 
-        <!--<br/>-->
+        
         <p>
             <a href="http://yo-yodesk.com/" target="_blank">
                 <img src="{{ url('/') }}/img/aw/summit/partners/yo-yo-new-logo.png">
 
-                <!--
-                <img src="{{ url('/') }}/img/oyf/partners/yo-yo1.jpg" width="150px">
-
-                <img src="{{ url('/') }}/img/oyf/partners/yo-yo.jpg" width="150px" height="70px">
-                -->
+                
             </a>
         </p>
     </div>
@@ -55,5 +91,6 @@
         <br/>
 
     </div>
+	-->
 
 </div>
