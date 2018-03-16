fb5bb0f8760adb1a67f513c38e5c7fa4662ad4ff
andrzej6 <andrzejdlugosz@yahoo.co.uk>
2018-03-16 15:53:36 +0000

    oyf competition changes1

--- resources/views/oyf/britain/competition.blade.php
+++ resources/views/oyf/britain/competition.blade.php
@@ -26,34 +26,81 @@
 
 @section('aw-maincontent')
 
-    <h1 class="oyf-newpink">Photo Competition | Win a free Yo-YoDesk®</h1>
+    <h1 class="oyf-newpink">Photo Competition | Win 20 Prizes from Yo-YoDesk®</h1>
 
 
     <br/>
     To celebrate <strong>On Your Feet Day</strong>, our official sponsor
-    <a href="https://sit-stand.com/" style="color:#ff3f72;font-weight: bold;text-decoration:underline" target="_blank">Sit-Stand.Com</a> is kindly
-    offering the 5 free Yo-YoDesks®  as prizes for the best "Get up offa that thing" PHOTOS of you and your co-workers!
-    What will you do?<br/><br/>
+    <a href="https://sit-stand.com/" style="color:#ff3f72;font-weight: bold;text-decoration:underline" target="_blank">Sit-Stand.Com</a> 
+	is very kindly offering us <span style="color:#57b8d9;font-weight: bold">20 Free Yo-YoDesk® Prizes</span>
+
+	to give away for the best <strong>"Get up offa that thing"</strong> PHOTOS posted on social media of you and your co-workers! <br/><br/>
 
     To enter simply post your photos on
-    <a href="https://www.facebook.com/onyourfeet.britain/" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Facebook</a> or
-    <a href="https://twitter.com/getGBstanding" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Twitter</a> using the
-    <span style="color:#57b8d9;font-weight: bold">#OYF17</span> or
-    <span style="color:#57b8d9;font-weight: bold">#OnYourFeetBritain</span> and
+    <a href="https://www.facebook.com/onyourfeet.britain/" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Facebook</a>,
+   <a href="https://uk.linkedin.com/" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">LinkedIn</a>,
+   <a href="https://www.instagram.com/?hl=en" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Instagram</a> or 	
+    <a href="https://twitter.com/getGBstanding" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Twitter</a>	
+	using
+    <span style="color:#57b8d9;font-weight: bold">#OYF18</span> or
+    <span style="color:#57b8d9;font-weight: bold">#OnYourFeetBritain</span> with
     <a href="https://twitter.com/getGBstanding?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">@getGBstanding</a>
-
+   before April 30th 2018. The winners will be selected from the most imaginative, funny and original captions posted. 
+   Think different to stand out for our judging panel to see you capture the spirit and objectives of 
+   <span style="color:#57b8d9;font-weight: bold">On Your Feet Day</span>.
+	
 
     <br/><br/><br/>
-    <span style="color:#57b8d9;font-weight: bold;font-size: 16px">Prizes for best photos:</span>
+	
+	
+    <span style="color:#ff3f72;font-weight: bold;font-size: 16px">Win 20 Prizes from Yo-YoDesk®</span>
     <br/><br/>
     <a href="https://sit-stand.com/desk-risers/120-yo-yo-desk-mini.html" target="_blank">
         <img src="https://sit-stand.com/1382-atmn_normal/yo-yo-desk-mini.jpg" width="150"></a>&nbsp;&nbsp;
-    <a href="https://sit-stand.com/desk-risers/120-yo-yo-desk-mini.html" target="_blank">
-        <img src="https://sit-stand.com/1384-atmn_normal/yo-yo-desk-mini.jpg" width="150"></a>&nbsp;&nbsp;
-    <a href="https://sit-stand.com/desk-risers/120-yo-yo-desk-mini.html" target="_blank">
-        <img src="https://sit-stand.com/1394-atmn_normal/yo-yo-desk-mini.jpg" width="150"></a>
+		
+	<a href="https://sit-stand.com/desk-risers/156-yo-yo-desk-go1.html" target="_blank">
+	<img src="https://sit-stand.com/1450-atmn_normal/yo-yo-desk-go1.jpg" width="150"></a>&nbsp;&nbsp;
+	
+	<a href="https://sit-stand.com/standard-comfort-mats/164-yo-yo-mat.html" target="_blank">
+	<img src="https://sit-stand.com/1605-atmn_normal/yo-yo-mat.jpg" width="150"></a>&nbsp;&nbsp;
+	
+	<a href="https://sit-stand.com/desk-risers/123-yo-yo-desk-90.html#/yo_yodesk90_colour-white" target="_blank">
+	<img src="https://sit-stand.com/1400-atmn_normal/yo-yo-desk-90.jpg" width="150"></a>&nbsp;&nbsp;
+		
     <br/><br/>
-
+	
+	
+	 <span style="color:#57b8d9;font-weight: bold;font-size: 16px;">Best Groups:</span><br/><br/>
+	 
+	2 x <a href="https://sit-stand.com/desk-risers/120-yo-yo-desk-mini.html" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">
+	Yo-YoDesk MINI </a>(Black)<br/>
+	2 x <a href="https://sit-stand.com/desk-risers/123-yo-yo-desk-90.html" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Yo-YoDesk 90</a> (Black)<br/>
+	2 x <a href="https://sit-stand.com/desk-risers/156-yo-yo-desk-go1.html" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Yo-YoDesk GO</a> (Black / White)<br/>
+	6 x <a href="https://sit-stand.com/standard-comfort-mats/164-yo-yo-mat.html" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Yo-Yo Mat</a> (Standard / Large Size)<br/>
+	<br/>
+	
+	<span style="color:#57b8d9;font-weight: bold;font-size: 16px;">Best Females:</span><br/><br/>
+	 
+	1 x <a href="https://sit-stand.com/desk-risers/120-yo-yo-desk-mini.html" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">
+	Yo-YoDesk MINI </a>(Black)<br/>
+	3 x <a href="https://sit-stand.com/standard-comfort-mats/164-yo-yo-mat.html" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Yo-Yo Mat</a> (Standard / Large Size)<br/>
+	<br/>
+	
+	
+	
+	<span style="color:#57b8d9;font-weight: bold;font-size: 16px;">Best Groups:</span><br/><br/>
+	 
+	1 x <a href="https://sit-stand.com/desk-risers/120-yo-yo-desk-mini.html" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">
+	Yo-YoDesk MINI </a>(Black)<br/>
+	3 x <a href="https://sit-stand.com/standard-comfort-mats/164-yo-yo-mat.html" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Yo-Yo Mat</a> (Standard / Large Size)<br/>
+	<br/><br/><br/>
+	
+	
+	
+	
+	<a href="http://onyourfeetday.com/britain/winners" style="color:#ff3f72;font-weight: bold;font-size: 16px;font-style:underline">Win 20 Prizes from Yo-YoDesk®</a>
+	
+<!--
     <div style="display: inline-block;width:100px">Best Group:</div>
     3 x <a href="https://sit-stand.com/desk-risers/120-yo-yo-desk-mini.html" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">
      Yo-YoDesk MINI </a>(BLACK)<br/>
@@ -65,27 +112,30 @@
     <div style="display: inline-block;width:100px">Best Male:</div>
     1 x <a href="https://sit-stand.com/desk-risers/120-yo-yo-desk-mini.html" style="color:#1136d3;font-weight: bold; text-decoration:underline" target="_blank">
          Yo-YoDesk MINI </a>(BLACK)<br/><br/>
-
+-->
 
     <strong>What will you do?</strong> We hope to see lots of variety of fun and simple activities to
     <strong>#SitLess</strong> and <strong>#MoveMore</strong> at work on Friday 28th April.<br/><br/><br/>
 
 
-    <span style="color:#57b8d9;font-weight: bold;font-size: 16px;">Terms and Conditions:</span>
+    <span style="color:#ff3f72;font-weight: bold;font-size: 16px;">Terms and Conditions:</span>
 
     <ol style="margin-top:20px!important">
     <li>Prizes will be awarded for the BEST images posted on Social Media with
-        #OYF17 #OnYourFeetBritain @getGBstanding. There are three prize draw categories: 1) Best Group,
-        2) Best Male and 3) Best Female</li>
+        #OYF17 #OnYourFeetBritain @getGBstanding 
+		( <a href="https://www.facebook.com/onyourfeet.britain/" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Facebook</a>,
+   <a href="https://uk.linkedin.com/" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">LinkedIn</a>,
+   <a href="https://www.instagram.com/?hl=en" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Instagram</a> or 	
+    <a href="https://twitter.com/getGBstanding" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Twitter</a>). There are three prize draw categories: 1) Best Group,
+        2) Best Male and 3) Best Females</li>
 
-    <li>The best images in all three categories will be awarded a Yo-YoDesk MINI (Black).
-        The Best Group winner will receive three Yo-YoDesk MINI (Black).</li>
+    <li>The best images in all three categories will be awarded a prize or prize combination.</li>
 
     <li>You can enter as many times as you like in as each of the categories.</li>
 
     <li>The winning individuals will be announced via email and we may have to contact you via
         your Twitter or Facebook profile to provide us your contact details by
-        Wednesday 3rd May 2017. Winners will need to respond within 10 days to claim their prize.</li>
+        Wednesday 4th May 2018. Winners will need to respond within 10 days to claim their prize.</li>
 
     <li>The prize winners may be required to participate in publicity and promotional activities.</li>
 
