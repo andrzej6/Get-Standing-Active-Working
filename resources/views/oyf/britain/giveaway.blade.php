b1a31b2baddc73d71f4bebef7decb0f7b24572b7
andrzej6 <andrzejdlugosz@yahoo.co.uk>
2019-02-28 12:12:45 +0000

    giveaway page and styles

--- resources/views/oyf/britain/giveaway.blade.php
+++ resources/views/oyf/britain/giveaway.blade.php
@@ -25,10 +25,55 @@
 
 @section('aw-maincontent')
 
-    <h1 class="oyf-newpink">New Partnerships</h1>
+    <h1 class="oyf-newpink">Sit-Stand.Com® £25,000 Giveaway | Photo Competition</h1>
     <br/>
 
+	To celebrate <strong>On Your Feet Day</strong>, our official sponsor <a href="https://sit-stand.com/" class="oyf-newpink" target="_blank">Sit-Stand.Com®</a> is running an 
+	amazing <strong>£25,000 Giveaway</strong> of active office solutions.<br/><br/>
+
+    To qualify all you need to do is register for On Your Feet 2019 and you will receive a £50 voucher code from Sit-Stand.Com, to be used before 26th April 2019.<br/><br/>
+
+    And the best "<strong>Get up offa that thing</strong>" PHOTOS posted on social media will be awarded vouchers of £500 for the winner and £250 x 3 runner up prizes! <br/><br/>
+
+    To enter simply post your photos n Facebook, LinkedIn, Instagram or Twitter using #OYF19 or #OnYourFeetBritain with @getGBstanding before April 30th 2019. 
+	The winners will be selected from the most imaginative, funny and original captions posted. Think different to stand out for our judging panel to see 
+	you capture the spirit and objectives of On Your Feet Day. 
+	
+	To enter simply post your photos on
+    <a href="https://www.facebook.com/onyourfeet.britain/" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Facebook</a>,
+   <a href="https://uk.linkedin.com/" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">LinkedIn</a>,
+   <a href="https://www.instagram.com/?hl=en" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Instagram</a> or 	
+    <a href="https://twitter.com/getGBstanding" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">Twitter</a>	
+	using
+    <span style="color:#57b8d9;font-weight: bold">#OYF19</span> or
+    <span style="color:#57b8d9;font-weight: bold">#OnYourFeetBritain</span> with
+    <a href="https://twitter.com/getGBstanding?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" style="color:#1136d3;font-weight: bold;text-decoration:underline" target="_blank">@getGBstanding</a>
+   <strong>before April 30th 2019</strong>. The winners will be selected from the most imaginative, funny and original captions posted. 
+   Think different to stand out for our judging panel to see you capture the spirit and objectives of
+   <span style="color:#57b8d9;font-weight: bold">On Your Feet Day</span>.
+	
+	<br/><br/>
+
+	<span style="font-weight: bold;font-size: 16px" class="oyf-newpink">Sit-Stand.Com® £25,000 Giveaway </span><br/><br/>
     
+	<span style="font-weight: bold;font-size: 16px" class="oyf-newblue">Best Group:</span><br/><br/>
+        <div class="oyf-fwidth inblock">Winner</div><div class="inblock">£500</div>
+		<div class="oyf-fwidth inblock">Runner-up x 3</div><div class="inblock">£250 each </div>
+		<br/><br/>
+		 		
+
+		<span style="font-weight: bold;font-size: 16px" class="oyf-newblue">Best Female:</span><br/><br/>
+
+		<div class="oyf-fwidth inblock">Winner</div><div class="inblock">£500</div>
+		<div class="oyf-fwidth inblock">Runner-up x 3</div><div class="inblock">£250 each </div>
+        <br/><br/>
+
+		<span style="font-weight: bold;font-size: 16px" class="oyf-newblue">Best Male:</span><br/><br/>
+
+		<div class="oyf-fwidth inblock"></div><div class="inblock"></div>
+		<div class="oyf-fwidth inblock">Runner-up x 3</div><div class="inblock">£250 each </div>
+		
+
    
 
 
