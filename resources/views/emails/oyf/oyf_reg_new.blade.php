1a76bf879e6118714a9d3e871c6f48abc99c110d
andrzej6 <andrzejdlugosz@yahoo.co.uk>
2019-03-21 08:36:17 +0000

    oyf email change

--- resources/views/emails/oyf/oyf_reg_new.blade.php
+++ resources/views/emails/oyf/oyf_reg_new.blade.php
@@ -13,14 +13,24 @@
     <span style="font-size:12.0pt;">Thank you for registering for <strong>On Your Feet Day</strong> taking place:
     <span  style="color:#57b8d9;font-weight: bold">Friday 26th April 2019</span>,
     in your office and all around the world.
-    <br/>
+    <br/><br/>
 	
-	
-	
-	<span style="color:#da3b44">Password to access the posters: "<strong>OYF19</strong>"</span>
-    
+	<div style="font-weight:bold;font-size:19px;text-align:center;color:#ff3f72">Password to access the posters: "<strong>OYF19</strong>"</div>
 	<br/>
 
+    
+	To celebrate, <a style="color:#57b8d9;text-decoration:underline" href="https://sit-stand.com/">Sit-Stand.Com®</a> our official sponsor have a generous <span style="color:#57b8d9;font-weight:bold">£25,000 
+    Giveaway offer</span> for active office solutions. The first 500 users receive <span style="color:#57b8d9;font-weight:bold">£50 OFF</span> any purchase*.<br/><br/>
+
+    Below is your <span style="color:#ff3f72;font-weight:bold">£50 OFF* voucher code</span> from <a style="color:#57b8d9;text-decoration:underline" href="https://sit-stand.com/">Sit-Stand.Com®</a>. 
+	Please remember that this code <span style="text-decoration:underline">expires on 26th April 2019</span><br/><br/>
+
+    <div style="font-weight:bold;font-size:19px;text-align:center"><span style="color:#57b8d9">Voucher Code:</span>  <span style="color:#ff3f72">OYF50</span></div>
+  <br/>
+    * Minimum order value of £200. Varidesk & Ergotron products excluded from this offer. One voucher code per order
+
+<br/><br/>
+	
 
      <!--
 	 UK posters and communication materials are available for download using the password
