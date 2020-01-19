e6f23669e05c2c41f8e7ac7b51a0c2479ed3d5d9
sst-support <andrzejdlugosz678@gmail.com>
2020-01-19 20:28:38 +0000

    takeoff oyf19 banners

--- resources/views/layouts/oyf/can.blade.php
+++ resources/views/layouts/oyf/can.blade.php
@@ -34,11 +34,15 @@ $sidemenu=array('index'=>'Event','partners'=>'Partners','downloads'=>'Downloads'
         <div class="module_content slider_type1">
             <div id="banner1" class="OneByOne_container">
 			
-			     <div class="oneByOne_item ls_bg1">
+			     
+				
+				<!--
+				
+				<div class="oneByOne_item ls_bg1">
                     <img src="{{ url('/') }}/img/oyf/thankyou-can.jpg" alt="" class="go-to-onyourfeet-r go-to">
                 </div>
 				
-				<!--
+				
 
                 <div class="oneByOne_item ls_bg1">
                     <img src="{{ url('/') }}/img/oyf/can/slider/oyf18-andy.jpg" alt="" class="go-to-onyourfeet-download-can">
