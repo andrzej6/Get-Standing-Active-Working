faafa0a1472afb0802d887aad6f9cbe58a00c98a
sst-support <andrzejdlugosz678@gmail.com>
2020-01-19 20:19:54 +0000

    remove info-aw from usa reg notifications

--- app/Jobs/UsaSend2Emails.php
+++ app/Jobs/UsaSend2Emails.php
@@ -46,7 +46,7 @@ class UsaSend2Emails extends Job implements ShouldQueue
         Mail::send('emails.gusa.gusa_reg_notify', $array, function ($message) use ($tosend){
             $message->from('registrations@activeworking.com', 'Active Working');
             $message->to('web@activeworking.com');
-            $message->cc('info@activeworking.com');
+            //$message->cc('info@activeworking.com');
             $message->subject('GetAmericaStanding.Com | General Enquiry');
         });
     }
