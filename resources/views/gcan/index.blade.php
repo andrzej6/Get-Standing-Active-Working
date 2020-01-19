9d5edae50fdde035980888e761c0268f44045a6a
sst-support <andrzejdlugosz678@gmail.com>
2020-01-19 20:24:17 +0000

    takeoff oyf19 banners

--- resources/views/gcan/index.blade.php
+++ resources/views/gcan/index.blade.php
@@ -38,10 +38,11 @@
                 <div class="module_content slider_type1">
                     <div id="banner1" class="OneByOne_container">
 
-                        
+                        <!--
                         <div class="oneByOne_item ls_bg1">
                             <img src="img/getstanding/can/slider/big/oyf19-can.jpg" alt="" class="go-to go-to-oyf">
                         </div>
+						-->
                         
 
                         <div class="oneByOne_item ls_bg1">
