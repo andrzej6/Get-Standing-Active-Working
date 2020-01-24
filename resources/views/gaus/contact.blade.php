ad9bffbeff72d75b0a7409c9df4f76845d63e6d6
andrzej6 <andrzejdlugosz@yahoo.co.uk>
2020-01-24 10:56:50 +0000

    captcha aus

--- resources/views/gaus/contact.blade.php
+++ resources/views/gaus/contact.blade.php
@@ -3,6 +3,30 @@
 @section('header-styles')
     @parent
     <link rel="stylesheet" type="text/css" href="css/gaus/contact.css">
+	
+	<script type="text/javascript">
+			var allowSubmit = false;
+			function capcha_filled () {
+				allowSubmit = true;
+			}
+			function capcha_expired () {
+				allowSubmit = false;
+			}
+			var onloadCallback = function() {
+				grecaptcha.render('html_element', {
+					'sitekey':'6LenhkcUAAAAAPJPkm-N_Z2e7Jh_R0RayRjHPtDX',
+					'callback': capcha_filled,
+					'expired-callback': capcha_expired
+				});
+			};
+			function check_if_capcha_is_filled () {
+				if(allowSubmit) return true;
+				alert('Please check the capcha!');
+				return false;
+			}
+			</script>
+	 <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer></script>
+	
 @endsection
 
 @section('description')
@@ -47,7 +71,7 @@
         @endif
 
 
-        {!!  Form::open(array('url' => 'gaus_reg','class' => 'register_form')) !!}
+        {!!  Form::open(array('url' => 'gaus_reg','class' => 'register_form','onsubmit' => 'return check_if_capcha_is_filled();')) !!}
 
         {!!  Form::token() !!}
 
@@ -194,6 +218,12 @@
                 {!!   Form::textarea('fquest', old('fquest'), array('id' => 'fquest','cols'=>40,'rows'=>6)) !!}
                 <div style="clear: both;"></div>
             </li>
+			 <li style="margin-top: 10px;">
+                <label for="fquest"></label>
+                <div id="html_element" style="display:inline-block"></div>			
+                <div style="clear: both;"></div>
+            </li>
+			
 
             <li>
                 <button class="submit" type="submit" id="yellsub">Submit</button>
